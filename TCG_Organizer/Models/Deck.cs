72182c04b11b23cc7f58b1a7f07ce76bf2bb4fe3
Josh Scott <joshscott82@gmail.com>
2022-11-23 12:56:47 -0500

    adding deckdetail

--- TCG_Organizer/Models/Deck.cs
+++ TCG_Organizer/Models/Deck.cs
@@ -6,13 +6,17 @@ public class Deck
 {
 
     public int Id { get; set; }
+
     [StringLength(30)]
     public string DeckName { get; set; } = "NEW";
 
     public int TotalCards { get; set; }
+
     public int DeckColors { get; set; } //show the available cards in the color scheme selected
+
     [StringLength(125)]
     public string? Description { get; set; }
+
 
     public int UserId { get; set; }
     public virtual User? User { get; set; }
---
+++ TCG_Organizer/Models/DeckDetail.cs
@@ -0,0 +1,16 @@
+﻿namespace TCG_Organizer.Models;
+
+public class DeckDetail
+{
+
+    public int Id { get; set; }
+
+    public int DeckId { get; set; }
+    public virtual Deck? Deck { get; set; }
+
+    public int CardId { get; set; }
+    public virtual Card? Card { get; set; }
+
+    public int Quantity { get; set; }
+
+}
